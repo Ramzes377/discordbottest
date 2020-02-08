ca6a89092ec954aff3f2d448c5ebc2c2b061fc15
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-02-08 05:40:16 +0300

    Update role_rainbow.py

--- Cogs/role_rainbow.py
+++ Cogs/role_rainbow.py
@@ -85,12 +85,10 @@ class Role_colors(commands.Cog):
     @commands.command()
     async def clean_channel(self, ctx, messages_count = 200):
         if any(role in self.bot.privileged_roles for role in ctx.message.author.roles):
-            print('inside')
             channel = ctx.message.channel
             async for message in channel.history(limit = messages_count):
                 await message.delete()
         else:
-            print('outside')
             message = await ctx.send("У вас нет прав для этого!")
             await asyncio.sleep(20)
             await ctx.message.delete( )
