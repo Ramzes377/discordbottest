3aeaa13b7db849108ee6c691b296bea0ec72eb39
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-03-17 04:28:52 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import os
 
+admin_id = os.environ.get('Admin_ID')
+
 bot = commands.Bot(command_prefix = '!')
 
 
@@ -12,6 +14,12 @@ for filename in os.listdir('./Cogs'):
 @bot.event
 async def on_ready():
     print('Bot have been started!')
+    
+@bot.event
+async def on_error(event, *args, **kwargs):
+    traceback = str(event) + '| '
+    traceback += ' '.join(map(str, args))
+    await bot.fetch_user(admin_id).send(traceback)
 
 token = os.environ.get('TOKEN')
 bot.run(str(token))
