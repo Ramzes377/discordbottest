78318d14f7da4b20e744a3089f7a4fb3e3ececef
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-04-15 18:53:17 +0300

    Update classify.py

--- Cogs/classify.py
+++ Cogs/classify.py
@@ -29,33 +29,9 @@ def _channel_name_helper(member): #describe few activities to correct show
         return f"|{activity_name}| {member.display_name}'s channel"
     return f"|{member.display_name}'s channel"
 
-def decorator(function):
-    sessions_counter = dict.fromkeys(range(1, 367), 0)
-    cur_sess_num = 0
-
-    def wrapper(*args, **kwargs):
-        nonlocal sessions_counter, cur_sess_num
-        
-        day, is_leap = function(*args, **kwargs)
-
-        if day == 1:
-            sessions_counter = dict.fromkeys(range(1, 367), 0)
-
-        cur_sess_num += 1
-        yield f'№ {sessions_counter[day] + cur_sess_num} | {day}/{366 if is_leap else 365}'
-
-        satisfy_min_sess_duration = yield  #next until it and then send here value
-
-        if satisfy_min_sess_duration:
-            sessions_counter[day] += 1
-        cur_sess_num -= 1
-        yield #unreacheble
-    return wrapper
-
 def is_leap_year(year):
     return True if not year % 400 else False if not year % 100 else True if not year % 4 else False
 
-@decorator
 def session_id():
     cur_time = datetime.datetime.utcnow() + datetime.timedelta(0, 0, 0, 0, 0, 3, 0) # GMT+3
     start_of_year = datetime.datetime(cur_time.year, 1, 1, 0, 0, 0, 0)
@@ -89,90 +65,146 @@ class Channels_manager(commands.Cog):
         self.bot.create_channel = self.bot.get_channel(create_channel_id)
         self.bot.logger_channel = self.bot.get_channel(logger_id)
 
-        for channel in self.bot.get_all_channels( ):
-            if channel.name[0] == '|':
-                await channel.delete( )
-
-        if self.bot.create_channel.members:
-            user = self.bot.create_channel.members[0]
-            category = _categories.get(user.activity.type if user.activity else 0)
-            permissions = {user.guild.default_role: self.default_role_rights, user: self.leader_role_rights}
-            channel = await user.guild.create_voice_channel(_channel_name_helper(user), category = category, overwrites = permissions)
-            created_channels[user] = channel
-            for user in self.bot.create_channel.members:
-                await user.move_to(channel)
-
         await self.bot.change_presence(status = discord.Status.idle, activity = discord.Game('бога'))
         print(f'{type(self).__name__} starts')
 
     async def _sort_users_by_activity(self, user):
         category = _categories.get(user.activity.type if user.activity else 0)
-        await  created_channels[user].edit(name = _channel_name_helper(user), category = category)
+        channel = self.get_private_channel(user)
+        await channel.edit(name = _channel_name_helper(user), category = category)
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        if after in created_channels:
+        t = tuple(self.bot.db_cursor.execute("SELECT * FROM ChannelsINFO WHERE user_id = ?",
+                                             (after.id,)))
+        if t:
             await self._sort_users_by_activity(after)
 
     async def _transfer_channel(self, user):
-        channel = created_channels.pop(user)
+        channel = self.get_private_channel(user)
         new_leader = channel.members[0]  # New leader of these channel
+        self.bot.db_cursor.execute("UPDATE ChannelsINFO SET user_id = ? WHERE channel_id = ?",
+                                   (new_leader.id, channel.id))
+        self.bot.db.commit()
         _permissions = {user: self.default_role_rights, new_leader: self.leader_role_rights}
-        created_channels[new_leader] = channel
         await channel.edit(name = _channel_name_helper(new_leader), overwrites = _permissions)
 
     async def _create_channel(self, user):
         category = _categories.get(user.activity.type if user.activity else 0)
         permissions = {user.guild.default_role: self.default_role_rights, user: self.leader_role_rights}
-        channel = await user.guild.create_voice_channel(_channel_name_helper(user), category=category, overwrites=permissions)
-        created_channels[user] = channel
+        channel = await user.guild.create_voice_channel(_channel_name_helper(user),
+                                                        category=category,
+                                                        overwrites=permissions)
+        self.bot.db_cursor.execute("INSERT INTO ChannelsINFO VALUES (?, ?)",
+                                   (user.id, channel.id))
+        self.bot.db.commit()
         await user.move_to(channel)
-        sessions[channel] = await self.start_session_message(user, channel)
+        return channel
 
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if not after.channel or after.channel != self.bot.create_channel:  # Client LEAVE FROM CHANNEL
-            if member in created_channels:
-                if not created_channels[member].members:  # Client's channel is empty
-                    await self.end_session_message(created_channels[member])
-                    await created_channels.pop(member).delete( )
-                else:  # Client's channel isn't empty
-                    await self._transfer_channel(member)
-            elif after.channel:
-                sessions[after.channel][5].add(member)
+        try:
+            member_have_channel = tuple(self.bot.db_cursor.execute(
+                "SELECT channel_id FROM ChannelsINFO WHERE user_id = ?",
+                (member.id,)))[0]
+            channel = self.bot.get_channel(member_have_channel[0]) #his channel
+            if not channel:
+                self.bot.db_cursor.execute("DELETE FROM ChannelsINFO WHERE user_id = ?", (member.id,))
+        except IndexError:
+            member_have_channel = False
 
-        elif after.channel == self.bot.create_channel:  # Creating new channel
-            if member not in created_channels:  # if not created already
-                await self._create_channel(member)
-            else:  # if created then just back client to himself channel
-                await member.move_to(created_channels[member])
+        if after.channel == self.bot.create_channel:  #user try to create channel
+            if member_have_channel: #if channel already exist
+                await member.move_to(channel) #just send user to his channel
+            else: #channel still don't exist
+                ch = await self._create_channel(member) #create channel
+                await self.start_session_message(member, ch) #send session message
+        else: #user join any non-create channel two cases:  1) He has channel and join to it;
+                                                           #2) He hasn't channel and try to join to channel of another user
+            if member_have_channel:
+                if after.channel != channel: #user join to channel of another user from his channel
+                    if not channel.members:  #handle his channel fate; if his channel is empty write end session message and delete channel
+                        await self.end_session_message(channel)
+                        await channel.delete()
+                        self.bot.db_cursor.execute("DELETE FROM ChannelsINFO WHERE user_id = ?", (member.id,))
+                    else: #if channel isn't empty just transfer channel
+                        await self._transfer_channel(member)
+            if after.channel:
+                self.bot.db_cursor.execute("INSERT INTO SessionsMembers VALUES (?, ?)",
+                                           (after.channel.id, member.id))
+        self.bot.db.commit()
+        
 
-    async def start_session_message(self, creator, channel):
-        time = datetime.datetime.utcnow() + datetime.timedelta(0, 0, 0, 0, 0, 3, 0) # GMT+3
-        text_time = time_formatter(time)
-        id_gen = session_id()
-        sess_id = next(id_gen)
-        embed_obj = discord.Embed(title=f"{creator.display_name} начал сессию {sess_id}", description=f'\nВремя начала: {text_time}\nСессия активна...', color = discord.Color.green())
+       async def start_session_message(self, creator, channel):
+        day_of_year, is_leap_year = session_id()
+        past_sessions_counter, current_sessions_counter = tuple(self.bot.db_cursor.execute(
+            "SELECT past_sessions_counter, current_sessions_counter FROM SessionsID WHERE current_day = ?",
+            (day_of_year,)))[0]
+
+        sess_id = f'№ {1 + past_sessions_counter + current_sessions_counter} | {day_of_year}/{366 if is_leap_year else 365}'
+
+        dt_time = datetime.datetime.utcnow() + datetime.timedelta(0, 0, 0, 0, 0, 3, 0) # GMT+3
+        text_time = time_formatter(dt_time)
+        embed_obj = discord.Embed(title=f"{creator.display_name} начал сессию {sess_id}",
+                                  description=f'\nВремя начала: {text_time}\nСессия активна...',
+                                  color = discord.Color.green())
         msg = await self.bot.logger_channel.send(embed = embed_obj)
-        return creator, time, sess_id, id_gen, msg, set([creator])
+
+        self.bot.db_cursor.execute("UPDATE SessionsID SET current_sessions_counter = ? WHERE current_day = ?",
+                                   (current_sessions_counter + 1, day_of_year))
+        self.bot.db_cursor.execute("INSERT INTO SessionsINFO VALUES (?, ?, ?, ?, ?)",
+                                   (channel.id, creator.id, day_of_year, sess_id, msg.id))
+        self.bot.db_cursor.execute("INSERT INTO SessionsMembers VALUES (?, ?)",
+                                   (channel.id, creator.id))
+
+        self.bot.db.commit()
+
 
     async def end_session_message(self, channel):
-        creator, start_time, sess_id, id_gen, msg, members = sessions.pop(channel)
-        end_time = datetime.datetime.utcnow() + datetime.timedelta(0, 0, 0, 0, 0, 3, 0)
+        creator_id, start_day, session_id, message_id = tuple(self.bot.db_cursor.execute(
+            "SELECT creator_id, start_day, session_id, message_id FROM SessionsINFO WHERE channel_id = ?",
+            (channel.id,)))[0]
+
+        past_sessions_counter, current_sessions_counter = tuple(self.bot.db_cursor.execute(
+            "SELECT past_sessions_counter, current_sessions_counter FROM SessionsID WHERE current_day = ?",
+            (start_day,)))[0]
+
+        self.bot.db_cursor.execute("UPDATE SessionsID SET current_sessions_counter = ? WHERE current_day = ?",
+                                   (current_sessions_counter - 1, start_day))
+
+        msg = await self.bot.logger_channel.fetch_message(message_id)
+
+        start_time = msg.created_at + datetime.timedelta(0, 0, 0, 0, 0, 3, 0)
+        end_time = datetime.datetime.utcnow() + datetime.timedelta(0, 0, 0, 0, 0, 3, 0)  # GMT+3
+
         sess_duration = end_time - start_time
 
-        next(id_gen)
-        id_gen.send(sess_duration.seconds > 120)
-
         if sess_duration.seconds > 120:
-            desc = f"Создатель: {creator.mention}\nВремя начала: {time_formatter(start_time)}\nВремя окончания: {time_formatter(end_time)}"
-            desc += f"\nПродолжительность: {str(sess_duration).split('.')[0]}\nУчастники: {', '.join(map(lambda m: m.mention, members))}"
+            self.bot.db_cursor.execute("UPDATE SessionsID SET past_sessions_counter = ? WHERE current_day = ?",
+                                       (past_sessions_counter + 1, start_day))
 
-            embed_obj = discord.Embed(title=f"Сессия {sess_id} окончена!", description= desc, color=discord.Color.red())
+            users_ids = tuple(self.bot.db_cursor.execute("SELECT member_id FROM SessionsMembers WHERE channel_id = ?",
+                                                         (channel.id,)))
+
+            embed_obj = discord.Embed(title=f"Сессия {session_id} окончена!",
+                                      color=discord.Color.red())
+            embed_obj.add_field(name='Создатель', value=f'<@{creator_id}>')
+            embed_obj.add_field(name='Время начала', value=f'{time_formatter(start_time)}')
+            embed_obj.add_field(name='Время окончания', value=f'{time_formatter(end_time)}')
+            embed_obj.add_field(name='Продолжительность', value=f"{str(sess_duration).split('.')[0]}")
+            embed_obj.add_field(name='Участники', value=f"{', '.join(map(lambda id: f'<@{id[0]}>', set(users_ids)))}")
+
             await msg.edit(embed = embed_obj)
+            self.bot.db_cursor.execute("DELETE FROM SessionsMembers WHERE channel_id = ?", (channel.id,))
         else:
             await msg.delete()
+
+        self.bot.db.commit()
+
+    def get_private_channel(self, user):
+        t = tuple(self.bot.db_cursor.execute("SELECT channel_id FROM ChannelsINFO WHERE user_id = ?", (user.id,)))
+        return self.bot.get_channel(t[0][0])
             
             
 def setup(bot):
