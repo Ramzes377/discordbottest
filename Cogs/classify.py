afdcba948eccaa68d1ccc2f4ef971491bbdaffd7
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-04-22 19:04:06 +0300

    Update classify.py

--- Cogs/classify.py
+++ Cogs/classify.py
@@ -83,9 +83,50 @@ class Channels_manager(commands.Cog):
             else:
                 self.bot.db_cursor.execute("DELETE FROM ChannelsINFO WHERE channel_id = ?", (channel_id,))
             self.bot.db.commit()
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="help to create your own channel"))
+
+        role_request = self.bot.get_channel(int(os.environ.get('Role_request'))
+        story = await role_request.history(limit=3).flatten()
+        if len(story) > 0:
+            self.msg = story[0]
+        else:
+            self.msg = await channel.send("Нажмите на иконку игры, чтобы получить соответствующую игровую роль!")
+
+        #await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="help to create your own channel"))
         print(f'{type(self).__name__} starts')
 
+    async def _edit_role_giver_message(self, emoji_id):
+        emoji = self.bot.get_emoji(emoji_id)
+        await self.msg.add_reaction(emoji)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.user_id != self.bot.user.id:
+            emoji = payload.emoji
+            self.bot.db_cursor.execute(f"SELECT application_id FROM CreatedEmoji WHERE emoji_id = {emoji.id}")
+            application_id = self.bot.db_cursor.fetchone()
+            if application_id:
+                self.bot.db_cursor.execute(f"SELECT role_id FROM CreatedRoles WHERE application_id = {application_id[0]}")
+                associated_role = self.bot.db_cursor.fetchone()
+                if associated_role:
+                    guild = self.bot.get_guild(payload.guild_id)
+                    member = guild.get_member(payload.user_id)
+                    role = guild.get_role(associated_role[0])
+                    await member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if payload.user_id != self.bot.user.id:
+            emoji = payload.emoji
+            self.bot.db_cursor.execute(f"SELECT application_id FROM CreatedEmoji WHERE emoji_id = {emoji.id}")
+            application_id = self.bot.db_cursor.fetchone()
+            if application_id:
+                self.bot.db_cursor.execute(f"SELECT role_id FROM CreatedRoles WHERE application_id = {application_id[0]}")
+                associated_role = self.bot.db_cursor.fetchone()
+                if associated_role:
+                    guild = self.bot.get_guild(payload.guild_id)
+                    member = guild.get_member(payload.user_id)
+                    role = guild.get_role(associated_role[0])
+                    await member.remove_roles(role)
 
     async def update_message_icon(self, app_id, channel_id):
         self.bot.db_cursor.execute(f"SELECT message_id FROM SessionsINFO WHERE channel_id = {channel_id}")
@@ -102,14 +143,11 @@ class Channels_manager(commands.Cog):
             msg_embed.set_thumbnail(url = thumbnail_url)
             await msg.edit(embed=msg_embed)
 
-
-
     async def _sort_channels_by_activity(self, user):
         category = _categories.get(user.activity.type if user.activity else 0)
         channel = self.get_private_channel(user)
         channel_name = _activity_name(user) + f" {user.display_name}'s channel"
         await channel.edit(name = channel_name, category = category)
-
 
     async def logging_activities(self, after):
         # Session activities logging
@@ -136,6 +174,26 @@ class Channels_manager(commands.Cog):
                 await self.update_message_icon(app_id, channel_id)
         self.bot.db.commit()
 
+    async def _create_activity_emoji(self, guild, app_id):
+        self.bot.db_cursor.execute(f'SELECT name, icon_url FROM ActivitiesINFO WHERE application_id = {app_id}')
+
+        name, thumbnail_url = self.bot.db_cursor.fetchone()
+
+        name = name.lower().replace(' ', '')
+        thumbnail_url = thumbnail_url[:-10]
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail_url) as resp:
+                content = await resp.read()
+
+        emoji = await guild.create_custom_emoji(name=name, image=content)
+
+        try:
+            self.bot.db_cursor.execute("INSERT INTO CreatedEmoji VALUES (?, ?)", (app_id, emoji.id))
+            self.bot.db.commit()
+            await self._edit_role_giver_message(emoji.id)
+        except:
+            pass
 
     async def link_roles(self, after):
         app_id = after.activity.application_id
@@ -148,6 +206,11 @@ class Channels_manager(commands.Cog):
             role = guild.get_role(created_role[1])  # get role
             if not role in after.roles:  # check user have these role
                 await after.add_roles(role)
+            self.bot.db_cursor.execute(f"SELECT * FROM CreatedEmoji WHERE application_id = {app_id}")
+            is_emoji_created = self.bot.db_cursor.fetchone()
+            if not is_emoji_created:
+                await self._create_activity_emoji(guild, app_id)
+
         else:
             role = await guild.create_role(name=role_name,
                                            permissions=guild.default_role.permissions,
@@ -157,7 +220,7 @@ class Channels_manager(commands.Cog):
             self.bot.db_cursor.execute("INSERT INTO CreatedRoles VALUES (?, ?)", (app_id, role.id))
             self.bot.db.commit()
             await after.add_roles(role)
-
+            await self._create_activity_emoji(guild, app_id)
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
@@ -170,7 +233,6 @@ class Channels_manager(commands.Cog):
 
             await self.logging_activities(after)
 
-
     async def _transfer_channel(self, user):
         channel = self.get_private_channel(user)
         new_leader = channel.members[0]  # New leader of these channel
@@ -179,7 +241,6 @@ class Channels_manager(commands.Cog):
         _permissions = {user: self.default_role_rights, new_leader: self.leader_role_rights}
         channel_name = _activity_name(new_leader) + f" {new_leader.display_name}'s channel"
         await channel.edit(name = channel_name, overwrites = _permissions)
-
 
     async def _create_channel(self, user):
         category = _categories.get(user.activity.type if user.activity else 0)
@@ -192,7 +253,6 @@ class Channels_manager(commands.Cog):
         self.bot.db.commit()
         await user.move_to(channel)
         return channel
-
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -224,7 +284,6 @@ class Channels_manager(commands.Cog):
                 self.bot.db_cursor.execute("INSERT INTO SessionsMembers VALUES (?, ?)", (after.channel.id, member.id))
         self.bot.db.commit()
 
-
     async def start_session_message(self, creator, channel):
         day_of_year, is_leap_year = session_id()
         self.bot.db_cursor.execute(
@@ -251,7 +310,6 @@ class Channels_manager(commands.Cog):
                                    (channel.id, creator.id))
         self.bot.db.commit()
 
-
     async def end_session_message(self, channel):
         self.bot.db_cursor.execute(f"SELECT creator_id, start_day, session_id, message_id FROM SessionsINFO WHERE channel_id = {channel.id}")
         temp = self.bot.db_cursor.fetchone()
@@ -274,7 +332,7 @@ class Channels_manager(commands.Cog):
 
         sess_duration = end_time - start_time
 
-        if sess_duration.seconds > 120:
+        if sess_duration.seconds > 4:
             self.bot.db_cursor.execute("UPDATE SessionsID SET past_sessions_counter = ? WHERE current_day = ?",
                                        (past_sessions_counter + 1, start_day))
 
@@ -282,28 +340,41 @@ class Channels_manager(commands.Cog):
 
             roles_ids = set(self.bot.db_cursor.execute(f"SELECT associate_role FROM SessionsActivities WHERE channel_id = {channel.id}"))
 
-            thumbnail_url = msg.embeds[0].thumbnail.url
+
 
             embed_obj = discord.Embed(title=f"Сессия {session_id} окончена!", color=discord.Color.red())
             #embed_obj.add_field(name = 'Создатель', value=f'<@{creator_id}>', inline=False)
-            creator = self.bot.get_user(creator_id)
-            embed_obj.set_footer(text = creator.name + " - Создатель сессии", icon_url=creator.avatar_url)
+            thumbnail_url = msg.embeds[0].thumbnail.url
+            if thumbnail_url:
+                embed_obj.set_thumbnail(url = thumbnail_url)
+
             embed_obj.add_field(name='Время начала', value=f'{time_formatter(start_time)}')
             embed_obj.add_field(name='Время окончания', value=f'{time_formatter(end_time)}')
             embed_obj.add_field(name='Продолжительность', value=f"{str(sess_duration).split('.')[0]}", inline=False)
             embed_obj.add_field(name='Участники', value=', '.join(map(lambda id: f'<@{id[0]}>', users_ids)), inline=False)
-            embed_obj.set_thumbnail(url = thumbnail_url)
-            if roles_ids:
-                embed_obj.add_field(name='Игровые сессии', value=', '.join(map(lambda id: f'<@&{id[0]}>', roles_ids)), inline=False)
+
+            creator = self.bot.get_user(creator_id)
+            embed_obj.set_footer(text=creator.name + " - Создатель сессии", icon_url=creator.avatar_url)
+
+            # if roles_ids:
+            #     embed_obj.add_field(name='Игровые сессии', value=', '.join(map(lambda id: f'<@&{id[0]}>', roles_ids)), inline=False)
 
             await msg.edit(embed = embed_obj)
             self.bot.db_cursor.execute(f"DELETE FROM SessionsMembers WHERE channel_id = {channel.id}")
             self.bot.db_cursor.execute(f"DELETE FROM SessionsActivities WHERE channel_id = {channel.id}")
+
+            for role_id in roles_ids:
+                self.bot.db_cursor.execute(f"SELECT application_id FROM CreatedRoles WHERE role_id = {role_id[0]}")
+                app_id = self.bot.db_cursor.fetchone()[0]
+                self.bot.db_cursor.execute(f"SELECT emoji_id FROM CreatedEmoji WHERE application_id = {app_id}")
+                emoji = self.bot.get_emoji(self.bot.db_cursor.fetchone()[0])
+                await msg.add_reaction(emoji)
+
+
         else:
             await msg.delete()
 
         self.bot.db.commit()
-
 
     def get_private_channel(self, user):
         self.bot.db_cursor.execute(f"SELECT channel_id FROM ChannelsINFO WHERE user_id = {user.id}")
@@ -312,3 +383,4 @@ class Channels_manager(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Channels_manager(bot))
+
