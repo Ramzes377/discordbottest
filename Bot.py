055a60834babefe439e15eb149e84c3298627299
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-04-25 19:06:51 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -24,14 +24,16 @@ for filename in os.listdir('./Cogs'):
 async def on_ready():
     bot.db = await aiopg.create_pool(dsn)
 
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS ChannelsINFO(user_id int, channel_id int)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS SessionsINFO(channel_id int, creator_id int, start_day int, session_id text, message_id int)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS SessionsID(current_day int, past_sessions_counter int, current_sessions_counter int)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS SessionsMembers(channel_id int, member_id int)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS SessionsActivities(channel_id int, associate_role int)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS CreatedRoles(application_id int UNIQUE, role_id int)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS ActivitiesINFO(application_id int,  icon_url text, name text)')
-    bot.db_cursor.execute('CREATE TABLE IF NOT EXISTS CreatedEmoji(application_id int UNIQUE, emoji_id int)')
+    async with bot.db.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute('CREATE TABLE IF NOT EXISTS ChannelsINFO(user_id bigint, channel_id bigint)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS SessionsINFO(channel_id bigint, creator_id bigint, start_day bigint, session_id text, message_id bigint)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS SessionsID(current_day bigint, past_sessions_counter bigint, current_sessions_counter bigint)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS SessionsMembers(channel_id bigint, member_id bigint)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS SessionsActivities(channel_id bigint, associate_role bigint)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS CreatedRoles(application_id bigint, role_id bigint)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS ActivitiesINFO(application_id bigint,  icon_url text, name text)')
+            await cur.execute('CREATE TABLE IF NOT EXISTS CreatedEmoji(application_id bigint, emoji_id bigint)')
 
     print('Bot have been started!')
 
