d5dbb2c13bed6baa7e731a5f1eaa0137e645db40
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-06-09 21:48:44 +0300

    Update classify.py

--- Cogs/classify.py
+++ Cogs/classify.py
@@ -88,7 +88,9 @@ class Channels_manager(commands.Cog):
 
         async with self.bot.db.acquire() as conn:
             async with conn.cursor() as cur:
-                bot_roles_num = await cur.execute(f"SELECT COUNT(1) FROM CreatedRoles")
+                await cur.execute(f"SELECT COUNT(1) FROM CreatedRoles")
+                bot_roles_num = await cur.fetchone()
+                print(bot_roles_num)
                 for role in roles:
                     if role.hoist:
                         print(role, role.position)
