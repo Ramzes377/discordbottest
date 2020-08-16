0f72c75229b9287cb3eeba4cab9a761c8de5bf4f
Ramzes377 <47265969+Ramzes377@users.noreply.github.com>
2020-08-17 00:52:17 +0300

    Update commands.py

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 from discord.ext import commands
-from .classify import _hash, time_formatter
+from .classify import _hash, get_app_id
 import asyncio_extras
 import datetime
 from time import time
@@ -15,56 +15,62 @@ class Commands(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{type(self).__name__} starts')
-
+        
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        if before.activity and before.activity.type == discord.ActivityType.playing:
-            sess_duration = int(time() - before.activity.start.timestamp() - 10800)
-            try:
-                app_id, is_real = before.activity.application_id, True
-            except:
-                app_id, is_real = _hash(before.activity.name), False
-
+        if after.activity and after.activity.type == discord.ActivityType.playing:
+            app_id, _ = get_app_id(after)
             async with self.get_connection() as cur:
                 await cur.execute(f"SELECT seconds FROM UserActivityDuration WHERE user_id = {before.id} AND app_id = {app_id}")
                 seconds = await cur.fetchone()
                 if not seconds:
-                    await cur.execute(f"INSERT INTO UserActivityDuration (user_id, app_id, seconds) VALUES ({before.id}, {app_id}, {sess_duration})")
-                    return
+                    await cur.execute(f"INSERT INTO UserActivityDuration (user_id, app_id, seconds) VALUES ({after.id}, {app_id}, {0})")
+
+        if before.activity and before.activity.type == discord.ActivityType.playing:
+            app_id, _ = get_app_id(before)
+            sess_duration = int(time() - before.activity.start.timestamp())
+            async with self.get_connection() as cur:
+                await cur.execute(f"SELECT seconds FROM UserActivityDuration WHERE user_id = {before.id} AND app_id = {app_id}")
+                seconds = await cur.fetchone()
                 await cur.execute(f"UPDATE UserActivityDuration SET seconds = {seconds[0] + sess_duration} WHERE user_id = {before.id} AND app_id = {app_id}")
 
-    @commands.command()
-    async def activity(self, ctx, game_role):
-        '''Дает пользователю продолжительность активности у соответствующей игре ИГРОВОЙ роли!
-                Введите !activity @роль чтобы получить продолжительность'''
-        member = ctx.message.author
-        requested_roles = ctx.message.role_mentions
 
-        if len(requested_roles) == 0:
-            await self.send_removable_message(ctx, 'Отсутствуют упоминания ролей! Введите !help activity', 20)
+    @commands.command()
+    async def activity(self, ctx):
+        '''Дает пользователю продолжительность активности у соответствующей игре ИГРОВОЙ роли!
+           Введите !activity @роль или Введите !activity @роль @роль . . .'''
+        member = ctx.message.author
+        channel = ctx.message.channel
+        requested_games = ctx.message.role_mentions
+
+        if len(requested_games) == 0:
+            await self.send_removable_message(ctx, 'Отсутствуют упоминания игровых ролей! Введите !help activity', 20)
             return
 
         async with self.get_connection() as cur:
-            for role in requested_roles:
+            for role in requested_games:
                 await cur.execute(f"SELECT application_id FROM CreatedRoles WHERE role_id = {role.id}")
                 app_id = await cur.fetchone()  # it's created role
                 if app_id:
                     await cur.execute(f"SELECT seconds FROM UserActivityDuration WHERE user_id = {member.id} AND app_id = {app_id[0]}")
                     seconds = await cur.fetchone()  # it's created role
                     total_time = datetime.timedelta(seconds=seconds[0])
-                    await cur.execute(f'SELECT icon_url FROM ActivitiesINFO WHERE application_id = {app_id[0]}')
-                    thumbnail_url = await cur.fetchone()
+
                     embed_obj = discord.Embed(title=f"В игре {role.name} вы провели:",
                                               color=role.color)
                     embed_obj.description = str(total_time).split('.')[0]
+
+                    await cur.execute(f'SELECT icon_url FROM ActivitiesINFO WHERE application_id = {app_id[0]}')
+                    thumbnail_url = await cur.fetchone()
                     if thumbnail_url:
                         embed_obj.set_thumbnail(url=thumbnail_url[0])
+
+                    bot = channel.guild.get_member(self.bot.user.id)
+                    embed_obj.set_footer(text=bot.display_name, icon_url=bot.avatar_url)
+
                     message = await member.send(embed = embed_obj)
                     await asyncio.sleep(30)
                     await message.delete()
-
-        await asyncio.sleep(15)
-        await ctx.message.delete()
 
 
     @commands.command()
